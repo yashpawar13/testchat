9421b3ad94e612d5cd03620292b73e1d968f052f
Yash Pawar <81626824+yashpawar13@users.noreply.github.com>
2025-07-11 15:47:07 +0530

    app.py
    test

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import numpy as np
 import streamlit as st
 import pickle
 
-model = pickle.load(open('CPurchasePredictorModel.pkl','rb'))
+model = pickle.load(open('PurchasePredictorModel.pkl','rb'))
 st.title("Customer Segmentation")
 
 age = int(st.text_input("Enter Age: ","18"))
